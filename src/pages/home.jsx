21168e3e9ed66576e8f0d32a86e40093b283a10c
sonu-virus <lallasonu36@gmail.com>
2024-08-07 13:13:48 +0530

    add coursal in home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,64 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import apiClient from "../apiClient/apiClient";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+//
+//
 const Home = () => {
-  return <div className="text-black">Hello</div>;
+  const [mPos, setMPos] = useState([]);
+  const moviePoster = async () => {
+    try {
+      const { data } = await apiClient.getMoviePoster();
+      console.log(data.results);
+      setMPos(data.results);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    moviePoster();
+  }, []);
+
+  return (
+    <div className="text-black">
+      <Swiper
+        modules={[Navigation, Pagination, Autoplay]}
+        spaceBetween={50}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+      >
+        <div>
+          {mPos?.map((data, i) => (
+            <SwiperSlide key={i}>
+              <div
+                className="bg-cover bg-center h-screen"
+                style={{
+                  backgroundImage: `url(https://image.tmdb.org/t/p/w500/${data.poster_path})`,
+                }}
+              >
+                <div className="w-full h-full bg-gradient-to-r from-black/50 to-transparent">
+                  <img
+                    className="w-full h-full object-contain"
+                    src={`https://image.tmdb.org/t/p/w500/${data.poster_path}`}
+                    alt={data.id}
+                  />
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </div>
+      </Swiper>
+    </div>
+  );
 };
 
 export default Home;
