739b5671e35d7411291f771eee47d702ecaf37f0
sonu-virus <lallasonu36@gmail.com>
2024-08-14 12:36:05 +0530

    now playing page added

--- src/pages/nowPlaying.jsx
+++ src/pages/nowPlaying.jsx
@@ -17,24 +17,34 @@ const NowPlaying = () => {
   }, []);
 
   return (
-    <div>
-      <div>Popular</div>
-      <div>
+    <div className="min-h-screen flex">
+      <div className="min-h-full w-48 bg-gray-500">
+        <h2>Now Playing Movies</h2>
+        <div>ilh</div>
+      </div>
+      <div className="flex justify-center items-center w-full">
         {nowPlay && nowPlay.length > 0 ? (
-          nowPlay.map((data, i) => (
-            <div key={data.id || i}>
-              <img
-                height={200}
-                width={300}
-                src={`https://image.tmdb.org/t/p/w500/${data.poster_path}`}
-                alt={data.original_title}
-              />
-
-              <div>{data.poster_path}</div>
-            </div>
-          ))
+          <div className="grid grid-cols-5 gap-2 p-2">
+            {nowPlay.map((data, i) => (
+              <div className="h-[430px] shadow-md shadow-gray-600/20 rounded-md">
+                <div className="h-[300px] w-full" key={data.id || i}>
+                  <img
+                    className="rounded-t-md w-full h-full"
+                    src={`https://image.tmdb.org/t/p/w500/${data.poster_path}`}
+                    alt={data.original_title}
+                  />
+                </div>
+                <div className="  p-4 rounded-b-md">
+                  <div className="font-bold flex-wrap">
+                    {data.original_title}
+                  </div>
+                  <div>{data.release_date}</div>
+                </div>
+              </div>
+            ))}
+          </div>
         ) : (
-          <div>No popular movies available</div>
+          <div>No nowPlaying movies available</div>
         )}
       </div>
     </div>
