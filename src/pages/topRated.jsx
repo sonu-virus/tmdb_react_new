bb0a9badf29b8ac26d9e5d1f6627b7b23930f6bd
sonu-virus <lallasonu36@gmail.com>
2024-08-14 12:36:31 +0530

    top rated page added

---
+++ src/pages/topRated.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import apiClient from "../apiClient/apiClient";
+
+const TopMovies = () => {
+  const [topMov, setTopMov] = useState([]);
+  const topMovies = async () => {
+    try {
+      const res = await apiClient.getTopRated();
+      console.log(res.data.results);
+      setTopMov(res.data.results);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    topMovies();
+  }, []);
+
+  return (
+    <div className="min-h-screen flex">
+      <div className="min-h-full w-48 bg-gray-500">
+        <h2>Now Playing Movies</h2>
+        <div>ilh</div>
+      </div>
+      <div className="flex justify-center items-center w-full">
+        {topMov && topMov.length > 0 ? (
+          <div className="grid grid-cols-5 gap-2 p-2">
+            {topMov.map((data, i) => (
+              <div className="h-[430px] shadow-md shadow-gray-600/20 rounded-md">
+                <div className="h-[300px] w-full" key={data.id || i}>
+                  <img
+                    className="rounded-t-md w-full h-full"
+                    src={`https://image.tmdb.org/t/p/w500/${data.poster_path}`}
+                    alt={data.original_title}
+                  />
+                </div>
+                <div className="  p-4 rounded-b-md">
+                  <div className="font-bold flex-wrap">
+                    {data.original_title}
+                  </div>
+                  <div>{data.release_date}</div>
+                </div>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div>No topMoving movies available</div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default TopMovies;
